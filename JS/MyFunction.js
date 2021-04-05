3e70b4f2f57804d996db6d12b4979a8340f841b9
Abraham <nencho_1@hotmail.com>
2021-04-05 10:31:29 +0200

    done all functions but i have problems with height decimals

--- JS/MyFunction.js
+++ JS/MyFunction.js
@@ -3,6 +3,8 @@ const userName   = document.querySelector('#name');
 const surname    = document.querySelector('#surname');
 const age        = document.querySelector('#age');
 const gender     = document.querySelector('#gender');
+const date1      = document.querySelector('#date1');
+const date2      = document.querySelector('#date2');
 const dni        = document.querySelector('#id');
 const height     = document.querySelector('#height');
 const email      = document.querySelector('#email');
@@ -28,7 +30,7 @@ form.addEventListener('submit', e => {
     e.preventDefault();
 });
 
-//A diferencia de keyup, change nos muestra los cambios en el form siempre, keyup si por ejemplo copias y pegas algo ene l input no lo detecta
+//A diferencia de keyup, change nos muestra los cambios en el form siempre, keyup si por ejemplo copias y pegas algo en el input no lo detecta
 userName.addEventListener('change', e => {
     const nameForm = e.target.value;
     const nameFormChangedFirstLetterToCapital = nameForm[0].toUpperCase() + nameForm.substr(1);
@@ -51,15 +53,33 @@ gender.addEventListener('change', e => {
     if(e.target.checked == true) formIsValid.gender = true;
 });
 
+date1, date2.addEventListener('change', e => {
+
+    const date1Formated = new Date(date1.value);
+    const date2Formated = new Date(date2.value);
+    console.log(date1Formated);
+    console.log(date2Formated);
+
+    let milisegDay = 24 * 60 * 60 * 1000;
+
+    let milisegSpendt = Math.abs(date1Formated.getTime() - date2Formated.getTime());
+
+    let daysSpendt = Math.round(milisegSpendt/milisegDay);
+
+    console.log(milisegDay);
+    console.log(milisegSpendt);
+    console.log(daysSpendt);
+});
+
 dni.addEventListener('change', e => {
     if(e.target.value.trim().length > 0) formIsValid.dni = true;
 });
 
 height.addEventListener('change', e => {
     const heightWithOnlyTwoDecimal = e.target.value;
-    const newNumber = heightWithOnlyTwoDecimal.toFixed(2)
+    const newNumber = heightWithOnlyTwoDecimal.toFixed();
     console.log(newNumber);
-    if(heightWithOnlyTwoDecimal.trim().length > 0) formIsValid.height = true;
+    if(newNumber.trim().length > 0) formIsValid.height = true;
 });
 
 email.addEventListener('change', e => {
@@ -67,10 +87,25 @@ email.addEventListener('change', e => {
 });
 
 observer.addEventListener('input', e => {
-    const textAreaForm = e.target.value;
+    //Esta es la funcion que habia buscado en Stack Overflow, la dejo como recuerdo y ejemplo adiccional
+    /* const textAreaForm = e.target.value;
     const textAreaValidate50FirstLetters = textAreaForm.substring( 0, 50);
-    console.log(textAreaValidate50FirstLetters)
-    if(textAreaValidate50FirstLetters.trim().length > 0) formIsValid.observer = true;
+    console.log(textAreaValidate50FirstLetters) */
+
+    //Esta es la forma que tu mandas en el ejercicio
+    let contentTextArea = '';
+    let maxLengtTextAreaContentAllowed = 50;
+
+    const realLenghtTextArea = e.target.value.length;
+    console.log(realLenghtTextArea)
+
+    if(realLenghtTextArea <= maxLengtTextAreaContentAllowed) {
+        contentTextArea = e.target.value;
+    }else {
+        e.target.value = contentTextArea;
+    }
+
+    if(contentTextArea.trim().length > 0 && contentTextArea.trim().length < 50 ) formIsValid.observer = true;
 });
 
 newsletter.addEventListener('change', e => {
